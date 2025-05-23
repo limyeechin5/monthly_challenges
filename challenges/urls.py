05fc71e0cb68a534021643f6d42ffd6b3807ff01
Yee Chin Lim <limyeechin5@gmail.com>
2025-05-23 10:49:01 +0800

    section 3.24

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january",  views.index) , 
-    path("february",  views.index2)
-
+    path("<month>" , views.monthly_challenges),
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 
 # Create your views here.
-def index(request):
-    return HttpResponse("This Works !!")
+def monthly_challenges(request,month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "JANUARY"
+    elif month == "february":
+        challenge_text = "FEBRUARY"
+    elif month == "march":
+        challenge_text = "MARCH"
+    elif month == "april":
+        challenge_text = "APRIL"
+    elif month == "may":
+        challenge_text = "MAY"
+    elif month == "june":
+        challenge_text = "JUNE"
+    elif month == "july":
+        challenge_text = "JULY"
+    elif month == "august":
+        challenge_text = "AUGUST"
+    elif month == "september":
+        challenge_text = "SEPTEMBER"
+    elif month == "october":
+        challenge_text = "OCTOBER"
+    elif month == "november":
+        challenge_text = "NOVEMBER"
+    elif month == "december":
+        challenge_text = "DECEMBER"
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+
+    return HttpResponse(challenge_text)  
+    
 
