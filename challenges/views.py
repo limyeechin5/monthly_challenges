44078313f989a331e54837a2118b77216ee1941b
Yee Chin Lim <limyeechin5@gmail.com>
2025-05-23 15:06:21 +0800

    section 3.26

--- challenges/views.py
+++ challenges/views.py
@@ -2,39 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
 
+monthly_challenge = {
+    "january" : "JANUARY" ,
+    "february" : "FEBRUARY" ,
+    "march" : "MARCH" ,
+    "april" : "APRIL" ,
+    "may" : "MAY" ,
+    "june" : "JUNE" ,
+    "july" : "JULY" ,
+    "august" : "AUGUST" ,
+    "september" : "SEPTEMBER" ,
+    "october" : "OCTOBER" ,
+    "november" : "NOVEMBER" ,
+    "december" : "DECEMBER" 
+}
+
 # Create your views here.
-def monthly_challenges_by_number(request,month):
+def monthly_challenges_by_number(request, month):
     return HttpResponse(month)
 
-def monthly_challenges(request,month):
-    challenge_text = None
-    if month == "january":
-        challenge_text = "JANUARY"
-    elif month == "february":
-        challenge_text = "FEBRUARY"
-    elif month == "march":
-        challenge_text = "MARCH"
-    elif month == "april":
-        challenge_text = "APRIL"
-    elif month == "may":
-        challenge_text = "MAY"
-    elif month == "june":
-        challenge_text = "JUNE"
-    elif month == "july":
-        challenge_text = "JULY"
-    elif month == "august":
-        challenge_text = "AUGUST"
-    elif month == "september":
-        challenge_text = "SEPTEMBER"
-    elif month == "october":
-        challenge_text = "OCTOBER"
-    elif month == "november":
-        challenge_text = "NOVEMBER"
-    elif month == "december":
-        challenge_text = "DECEMBER"
-    else:
-        return HttpResponseNotFound("This month is not supported!")
-
-    return HttpResponse(challenge_text)  
+def monthly_challenges(request, month):
+    try:
+        challenge_text = monthly_challenge[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month is not support")
+      
     
 
